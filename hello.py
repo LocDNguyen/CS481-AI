b84b0914985ec2c5ba3d8baf849191c4202b3731
Loc Nguyen <locdacnguyen0@gmail.com>
2023-11-29 20:44:30 -0800

    added code for scroll

--- hello.py
+++ hello.py
@@ -1,15 +1,14 @@
 from tkinter import *
 from maze import start, finish
+from scroll import *
 
-# Has to happen before anything else
-root = Tk()
+
+
 root.title("A* Maze Runner")
-#root.geometry("400x400")
 
 
-# Display maze
 def enter():
-    for label in root.grid_slaves():
+    for label in fTable.grid_slaves():
         if int(label.grid_info()["row"]) > 3:
             label.grid_forget()
 
@@ -19,60 +18,52 @@ def enter():
     r = 5
 
     # Display initial maze
-    testing = Label()
     begin = start(rows, cols, wall)
-    initial = Label(root, text="Initial Maze:").grid(pady=2, column=0)#columnspan=2)
+    Label(fTable, text="Initial Maze:").grid(pady=2, column=0)
     for row in begin:
-        Label(text=" ".join(map(str,row)), borderwidth=1).grid(pady=2, column=0)#columnspan=2)
+        Label(fTable, text=" ".join(map(str,row)), borderwidth=1).grid(pady=2, column=0)
+        updateScrollRegion()
 
     # Display A* maze
     final = finish(rows, cols, begin)
     if final == "None":
-        short_path = Label(root, text="No valid path found.").grid(row=4, column=1)#columnspan=2)
+        Label(fTable, text="No valid path found.").grid(row=4, column=1)
     else:
-        short_path = Label(root, text="Shortest Path:").grid(pady=2, row=4, column=1)#columnspan=2)
+        Label(fTable, text="Shortest Path:").grid(pady=2, row=4, column=1)
         for row in final:
-            Label(text=" ".join(map(str,row)), borderwidth=1).grid(pady=2, row=r, column=1)#columnspan=2)
+            Label(fTable, text=" ".join(map(str,row)), borderwidth=1).grid(pady=2, row=r, column=1)
             r += 1
-    # for label in root.grid_slaves():
-    #     Label(text="" + str(label.grid_info()), borderwidth=1).grid(columnspan=2)
+            updateScrollRegion()
 
-# Delete maze variables and the maze
 def clear():
     num_of_rows.delete(0, END)
     num_of_cols.delete(0, END)
     wall_prob.delete(0, END)
-    for label in root.grid_slaves():
+    for label in fTable.grid_slaves():
         if int(label.grid_info()["row"]) > 3:
             label.grid_forget()
 
+
 # Create text boxes
-num_of_rows = Entry(root, width=20)
+num_of_rows = Entry(fTable, width=20)
 num_of_rows.grid(row=0, column=1, padx=20)
-num_of_cols = Entry(root, width=20)
+num_of_cols = Entry(fTable, width=20)
 num_of_cols.grid(row=1, column=1, padx=20)
-wall_prob = Entry(root, width=20)
+wall_prob = Entry(fTable, width=20)
 wall_prob.grid(row=2, column=1, padx=20)
 
 # Create text box labels
-rows_label = Label(root, text="Number of Rows:")
-rows_label.grid(row=0, column=0, padx=20, pady=5)
-cols_label = Label(root, text="Number of Columns:")
-cols_label.grid(row=1, column=0, padx=20)
-wall_label = Label(root, text="Wall probability (Range: 0-1):")
-wall_label.grid(row=2, column=0, padx=20, pady=5)
+rows_label = Label(fTable, text="Number of Rows:").grid(row=0, column=0, padx=20, pady=5)
+cols_label = Label(fTable, text="Number of Columns:").grid(row=1, column=0, padx=20)
+wall_label = Label(fTable, text="Wall probability (Range: 0-1):").grid(row=2, column=0, padx=20, pady=5)
 
 # Create buttons for creating the maze and clearing everything
-pass_arguments = Button(root, text="Enter", command=enter, width=10, padx=5, pady=1)
-pass_arguments.grid(row=3, column=0, pady=10)
+pass_arguments = Button(fTable, text="Enter", command=enter, width=10, padx=5, pady=1).grid(row=3, column=0, pady=10)
 
-clear_arguments = Button(root, text="Clear", command=clear, width=10, padx=5, pady=1)
-clear_arguments.grid(row=3, column=1)
+clear_arguments = Button(fTable, text="Clear", command=clear, width=10, padx=5, pady=1).grid(row=3, column=1)
 
 # Allow keyboard enter key to create maze
 root.bind('<Return>',lambda event:enter())
 
-# Center the application window
-root.eval('tk::PlaceWindow . center')
 
 root.mainloop()
