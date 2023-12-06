52704a48499b410a3c7aa9e805c8a2a10d633b04
StudentAccount1000 <89495655+StudentAccount1000@users.noreply.github.com>
2023-12-06 00:07:55 -0800

    A* algo is now correct

--- Astar_gen.py
+++ Astar_gen.py
@@ -1,19 +1,9 @@
-from heapq import *
 from math import sqrt
 import random
 
 
 
-# def generate_maze(rows, cols, wall_prob):
-#     maze = [[0] * cols for _ in range(rows)]
-#     for row in range(rows):
-#         for col in range(cols):
-#             if row == 0 or col == 0 or row == rows - 1 or col == cols - 1 or random.random() < wall_prob:
-#                 maze[row][col] = 1  # 1 represents a wall
-#     maze[4][1] = 0
-#     #maze[rows-2][cols-2] = 0
-#     maze[8][15] = 0
-#     return maze
+
 def generate_maze(rows, cols, wall_prob, start_x, start_y, end_x, end_y):
     maze = [[0] * cols for _ in range(rows)]
     for row in range(rows):
@@ -47,36 +37,54 @@ def get_neighbors(node, rows, cols):
     neighbors = [(node[0] + 1, node[1]), (node[0] - 1, node[1]), (node[0], node[1] + 1), (node[0], node[1] - 1)]
     return [(r, c) for r, c in neighbors if 0 <= r < rows and 0 <= c < cols]
 
-#only removes 1 duplicate
-def remove_duplicates_from_open_set(open_set):
-    first = []
-    for i in range(len(open_set)):
-        if open_set[i][1] not in first:
-            first.append(open_set[i][1])
-        else:
-            open_set.pop(i)
-            break
-    heapify(open_set)
-    return open_set
+def coords_in_list(l, coords):
+    # print("entered coords_in_list with " + str(l) + " " + str(coords))
+    for cost, l_coords in l:
+        if l_coords == coords:
+            return True
+    return False
 
-def update_cost(open_set, node, cost):
-    for n in open_set:  #doesnt work
-        if n[1] == node:
-            n[0] = cost
-    heapify(open_set)   #the ordering may have changed
-    #remove duplicates
-    return remove_duplicates_from_open_set(open_set)
+def get_cost(l, coords):
+    for cost, l_coords in l:
+        if coords == l_coords:
+            return cost
+    return 9999999
+
+def drop_by_coords(l, coords):
+    for i in range(len(l)):
+        cost, l_coords = l[i]
+        if coords[0] == l_coords and coords[1] == l_coords:
+            l.pop(i)
+            #print("Dropped: " + str(l[i]))
+            return True
+    return False
+
+def insert_by_cost(l, cost, coords):
+    if len(l) == 0:
+        l.append((cost, coords))
+    largest = True
+    for i in range(len(l)):
+        if l[i][0] < cost:
+            continue
+        else:
+            l.insert(i, (cost, coords))
+            largest = False
+            break
+    
+    if largest:
+        l.append((cost, coords))
+    return l
+
 
 def astar_pathfind_gen(maze, start, goal):
     rows, cols = len(maze), len(maze[0])
-    open_set = []
-    heappush(open_set, (heuristic(start, goal), start))     #remove duplicates
+    open_set = [(heuristic(start, goal), start)]
     came_from = {start: None}
     explored = {}
     goal_found = False
     actual_cost = 0
     while open_set and not goal_found:
-        current_cost, current_node = heappop(open_set)
+        current_cost, current_node = open_set.pop(0)
         actual_cost = len(reconstruct_path(came_from, current_node))    #actual cost is length of path-1 (since start does not move)
         print(str(actual_cost-1) + " cost for " + str(current_node))    #print for debugging
         explored[current_node] = actual_cost
@@ -87,22 +95,45 @@ def astar_pathfind_gen(maze, start, goal):
         if current_node == goal:
             goal_found = True
             print("Goal found!")
-            
-        for neighbor in get_neighbors(current_node, rows, cols):
-            if maze[neighbor[0]][neighbor[1]] != 1 and neighbor not in explored.keys():
-                if neighbor not in open_set:    #only covers case where cost is same. Otherwise, it will been seen as unique, which is why the duplicate needs to be removed
-                    heappush(open_set, (actual_cost+heuristic(neighbor, goal), neighbor))
-                    remove_duplicates_from_open_set(open_set)
-                    came_from[neighbor] = current_node
-                elif open_set[neighbor] > actual_cost + heuristic(neighbor, goal):
-                    open_set = update_cost(open_set, neighbor, actual_cost+heuristic(neighbor, goal))
-                    came_from[neighbor] = current_node
         
-        #debugging
-        print("explored:", end=" ")
+
+        # print("Neighbors of " + str(current_node) + ": " + str(get_neighbors(current_node, rows, cols)))
+
+        print("After pop before additions/updates:\nexplored:", end=" ")
         print(explored.keys())
         print("frontier:", end=" ")
         print(open_set)
+
+        for neighbor in get_neighbors(current_node, rows, cols):
+            if maze[neighbor[0]][neighbor[1]] != 1 and neighbor not in explored.keys():
+                if not coords_in_list(open_set, neighbor):
+                    # print("Adding: " + str(neighbor) + " with cost: " + str(actual_cost+heuristic(neighbor, goal)))
+                    open_set = insert_by_cost(open_set, actual_cost+heuristic(neighbor, goal), neighbor)
+                    came_from[neighbor] = current_node
+
+                else:
+                    # coords are in list, need to check if cost function should be updated
+                    if get_cost(open_set, neighbor) > heuristic(neighbor, goal) + actual_cost:
+                        # print("Updating cost of: " + str(neighbor) + " to " + str(heuristic(neighbor, goal) + actual_cost) + " from " + str(get_cost(open_set, neighbor)))
+                        #remove current tuple with oldcost, neighbor coordinates MOVED
+                        #drop_by_coords(open_set, neighbor)
+                        open_set = insert_by_cost(open_set, actual_cost+heuristic(neighbor, goal), neighbor)
+                        came_from[neighbor] = current_node
+
+
+                # remove duplicate
+                first_instance = []
+                for i in range(len(open_set)):
+                    if open_set[i][1] not in first_instance:
+                        first_instance.append(open_set[i][1])
+                    else:
+                        open_set.pop(i)
+                        break
+                
+                
+
+        
+        
             
     # print("\nNo valid path found.")
     return None  # No path found
